96e5d3badde19937ea954775a2665ac56016a072
sami <ydjm1027@naver.com>
2019-09-14 20:09:18 +0900

    댓글쓰기
    댓글 보이기 해야함

--- app/src/main/java/com/example/diaryproject/Diary/PostActivity.java
+++ app/src/main/java/com/example/diaryproject/Diary/PostActivity.java
@@ -12,6 +12,7 @@ import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,6 +20,7 @@ import android.widget.Toast;
 import com.example.diaryproject.MainActivity;
 import com.example.diaryproject.R;
 import com.example.diaryproject.sign.SignInActivity;
+import com.example.diaryproject.sign.SignUpActivity;
 import com.muddzdev.styleabletoast.StyleableToast;
 
 import org.json.JSONArray;
@@ -51,12 +53,13 @@ public class PostActivity extends AppCompatActivity {
     TextView tNickname;
     TextView tDate;
     ImageView tImage;
+    EditText tComment;
     Button btn;
 
     String mJsonString;
 
-    String id;
     String nickname;
+    String comment;
 
 
     @Override
@@ -66,16 +69,116 @@ public class PostActivity extends AppCompatActivity {
 
         Intent GetIntent = getIntent();
         post_id = GetIntent.getExtras().getString("POSTID");
-        id = GetIntent.getExtras().getString("USERID");
         nickname = GetIntent.getExtras().getString("NICKNAME");
 
         ReadPost task = new ReadPost();
         task.execute(post_id);
 
+
+        btn = findViewById(R.id.Ok_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tComment = findViewById(R.id.comment_text);
+                comment = tComment.getText().toString();
+                InsertComment taskcomment = new InsertComment();
+                taskcomment.execute(nickname,post_id,comment);
+
+
+            }
+        });
+
     }
 
     // 댓글 달기
+    class InsertComment extends AsyncTask<String, Void, String> {
+        ProgressDialog progressDialog;
 
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(PostActivity.this,
+                    "Please Wait", null, true, true);
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            progressDialog.dismiss();
+            Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+            Log.d(TAG, "POST response  - " + result);
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String nickname = (String)params[0];
+            String post_id = (String)params[1];
+            String comment = (String)params[2];
+
+            String serverURL = getString(R.string.sever) + "/comment.php";
+            String postParameters = "nickname=" + nickname + "&post_id=" + post_id + "&comment=" + comment;
+
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.connect();
+
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "POST response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+
+                bufferedReader.close();
+
+
+                return sb.toString();
+
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+
+                return new String("Error: " + e.getMessage());
+            }
+
+        }
+    }
 
 
 
