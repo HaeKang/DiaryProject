b1791e28a229a67b71c4e236ece7d856eb47f026
sami <ydjm1027@naver.com>
2019-11-03 15:29:01 +0900

    calender dot event
    글쓴날 달력에 점 추가 event

--- app/src/main/java/com/example/diaryproject/Fragment/PageFour/PageFourFragment.java
+++ app/src/main/java/com/example/diaryproject/Fragment/PageFour/PageFourFragment.java
@@ -1,17 +1,27 @@
 package com.example.diaryproject.Fragment.PageFour;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
 import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.diaryproject.Fragment.EventDecorator;
 import com.example.diaryproject.R;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.CalendarMode;
@@ -19,14 +29,42 @@ import com.prolificinteractive.materialcalendarview.DayViewDecorator;
 import com.prolificinteractive.materialcalendarview.DayViewFacade;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.prolificinteractive.materialcalendarview.spans.DotSpan;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 
 public class PageFourFragment extends Fragment {
 
     Date today = new Date(System.currentTimeMillis());
+
+    ArrayList<String> mArrayList = new ArrayList<>();
+    HashSet<CalendarDay> dayArrayList = new HashSet<>();
+
+    String mJsonString;
+    private String TAG = "PHPTEST";
+    private static final String TAG_DATE ="date";
+    private static final String TAG_JSON="date";
 
     public PageFourFragment() {
         // Required empty public constructor
@@ -51,8 +89,11 @@ public class PageFourFragment extends Fragment {
         String id = getArguments().getString("user_id");    // user id
         String nickname = getArguments().getString("user_nick");    // user nickname
 
+        final MaterialCalendarView materialCalendarView = v.findViewById(R.id.four_calendar);
 
-        MaterialCalendarView materialCalendarView = v.findViewById(R.id.four_calendar);
+        getPostDate task = new getPostDate();
+        task.execute(id);
+
 
         materialCalendarView.state().edit()
                 .setFirstDayOfWeek(Calendar.SUNDAY)
@@ -66,7 +107,8 @@ public class PageFourFragment extends Fragment {
 
         materialCalendarView.addDecorators(
                 new SundayDecorator(),
-                new SaturdayDecorator());
+                new SaturdayDecorator()
+        );
 
         // 클릭 event
         materialCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
@@ -76,12 +118,22 @@ public class PageFourFragment extends Fragment {
             }
         });
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                materialCalendarView.addDecorators(
+                        new EventDecorator()
+                );
 
-
+            }
+        }, 2000);
 
 
         return v;
     }
+
+
+
 
     // 일요일 빨간색상
     public class SundayDecorator implements DayViewDecorator{
@@ -119,8 +171,167 @@ public class PageFourFragment extends Fragment {
         }
     }
 
+    // 오늘날짜 노랑색
+    public class TodayDecorator implements DayViewDecorator{
+
+        private final Calendar calendar = Calendar.getInstance();
+        CalendarDay today = CalendarDay.today();
+
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+            day.copyTo(calendar);
+            return today != null && day.equals(today);
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+            view.addSpan(new StyleSpan(Typeface.BOLD));
+            view.addSpan(new DotSpan(10, Color.BLACK));
+        }
+    }
+
+
     // 글 쓴 날에 점찍기 deco
+    public class EventDecorator implements DayViewDecorator {
+
+        public EventDecorator() {
+
+        }
+
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+            return dayArrayList.contains(day);
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+            view.addSpan(new DotSpan(10, Color.BLACK));
+        }
+    }
+
+
+    // 글 목록이 있는 date들 불러오기
+    private class getPostDate extends AsyncTask<String, Void, String> {
+
+        ProgressDialog progressDialog;
+        String errorString = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(getActivity(),
+                    "글을 불러옵니다", null, true, true);
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            progressDialog.dismiss();
+
+
+            if(result.equals("글이 없오") || result == null){
+
+            }
+            else {
+                mJsonString = result;
+                showResult();
+            }
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String id = (String)params[0];
+            String postParameters = "id=" + id;
+
+            String serverURL = getString(R.string.sever) + "/FindPostDate.php";
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.connect();
+
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "POST response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+
+                bufferedReader.close();
+
+
+                return sb.toString();
+
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+                errorString = e.toString();
+
+                return null;
+            }
+
+        }
+
+        private void showResult(){
+            try {
+                JSONObject jsonObject = new JSONObject(mJsonString);
+                JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
+                mArrayList.clear();
+
+                for(int i=0; i<jsonArray.length(); i++){
+                    JSONObject item = jsonArray.getJSONObject(i);
+
+                    String date = item.getString(TAG_DATE);
+                    SimpleDateFormat transFormat = new SimpleDateFormat("yyyy-MM-dd");
+                    Date realdate = transFormat.parse(date);
+                    dayArrayList.add(CalendarDay.from(realdate));
+
+                }
 
 
 
+            } catch (JSONException e) {
+
+                Log.d(TAG, "showResult : ", e);
+
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 }
